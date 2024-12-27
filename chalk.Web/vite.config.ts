35a74ec08c3a9c57d8ff8ab34f037d8de85ec7f2
ZhangTerrence <terrencezhang77@gmail.com>
2024-12-26 20:34:08 -0500

    chore: updated docker compose

--- chalk.Web/vite.config.ts
+++ chalk.Web/vite.config.ts
@@ -3,8 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/",
     plugins: [react()],
     server: {
+        port: 3000,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:8080",
+    },
+    preview: {
         port: 3000,
         strictPort: true
     }
