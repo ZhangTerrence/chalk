892559c117046078ab51f6f127751d6c7c4f65a5
ZhangTerrence <terrencezhang77@gmail.com>
2025-01-17 14:13:02 -0500

    fix: protected routes now waits for refresh

--- src/chalk.Web/src/components/AuthenticationGuard.tsx
+++ src/chalk.Web/src/components/AuthenticationGuard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import { Navigate, useNavigate } from "react-router-dom";
+import { LoaderIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 import { useLazyRefreshQuery } from "@/redux/services/account.ts";
 import { selectUser } from "@/redux/slices/user.ts";
@@ -8,26 +9,28 @@ import { useTypedSelector } from "@/redux/store.ts";
 
 export const AuthenticationGuard = ({ children }: { children: React.ReactNode }) => {
   const user = useTypedSelector(selectUser);
-  const [refresh] = useLazyRefreshQuery();
+  const [refresh, { isFetching, isError }] = useLazyRefreshQuery();
   const navigate = useNavigate();
 
-  const tryRefresh = async () => {
-    try {
-      await refresh(null).unwrap();
-    } catch (error) {
-      navigate("/login");
-    }
-  };
-
   React.useEffect(() => {
+    const tryRefresh = async () => {
+      await refresh(null).unwrap();
+    };
+
     if (!user) {
-      tryRefresh().then();
+      tryRefresh();
     }
   }, [user]);
 
-  if (!user) {
-    return <Navigate to="/login" />;
+  React.useEffect(() => {
+    if (!isFetching && isError) {
+      navigate("/login");
+    }
+  }, [isFetching, isError]);
+
+  if (isFetching) {
+    return <LoaderIcon className="absolute animate-spin" />;
   }
 
-  return children;
+  return user && children;
 };
