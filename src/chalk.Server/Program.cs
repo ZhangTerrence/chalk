ee81ce759b0ecd7c76b2a013d4990ad2099bb125
ZhangTerrence <terrencezhang77@gmail.com>
2025-02-05 14:24:39 -0500

    chore: added logging

--- src/chalk.Server/Program.cs
+++ src/chalk.Server/Program.cs
@@ -1,116 +1,55 @@
 using chalk.Server.Configurations;
 using chalk.Server.Data;
-using chalk.Server.Entities;
-using chalk.Server.Services;
-using chalk.Server.Services.Interfaces;
 using FluentValidation;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
-using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDataProtection().UseCryptographicAlgorithms(new AuthenticatedEncryptorConfiguration
-{
-    EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
-    ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
-});
-
-// Adds logging
-builder.Services.AddHttpLogging(_ => { });
-
-// Connects PostgreSQL database
+// Configures logging
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+// Configures PostgreSQL database
 builder.Services.AddDbContext<DatabaseContext>(options => options
-    .UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
-    .UseSnakeCaseNamingConvention());
+  .UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+  .UseSnakeCaseNamingConvention());
+
+// Configures CORS
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy", policy => policy
+  .SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+  .AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
+// Configures authentication and authorization
+builder.Services.AddAuth();
+// Configures controller behavior
 builder.Services
-    .AddIdentity<User, IdentityRole<long>>()
-    .AddEntityFrameworkStores<DatabaseContext>()
-    .AddDefaultTokenProviders();
-
-// Adds CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(
-        name: "CorsPolicy",
-        policy => policy
-            .SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials()
-    );
-});
-
-// Adds authentication and authorization
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    options.User.RequireUniqueEmail = true;
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequiredLength = 8;
-    options.SignIn.RequireConfirmedEmail = true;
-    options.Lockout.AllowedForNewUsers = true;
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 5;
-});
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.Cookie.HttpOnly = true;
-    options.Cookie.SameSite = SameSiteMode.Strict;
-    options.Cookie.MaxAge = TimeSpan.FromDays(1);
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(15);
-    options.LoginPath = "/api/account/refresh";
-});
-builder.Services.AddAuthorization();
-
-// Configures controller and routing behavior
-builder.Services
-    .AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true)
-    .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+  .AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true)
+  .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+// Configures routing behavior
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
-
-// Adds FluentValidation
+// Configures FluentValidation
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-
-// Adds scoped services
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ICourseService, CourseService>();
-builder.Services.AddScoped<IOrganizationService, OrganizationService>();
-builder.Services.AddScoped<IFileService, FileService>();
-builder.Services.AddScoped<ICloudService, CloudService>();
-builder.Services.AddScoped<IEmailService, EmailService>();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Configures scoped services
+builder.Services.AddScopedDependencies();
+// Configures data protection
+builder.Services.AddDataProtection();
+// Configures Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// Configures exception handling
 builder.Services.AddExceptionHandler<ExceptionHandler>();
 builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
 // Configures the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+if (app.Environment.IsDevelopment()) {
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
-
-app.UseHttpLogging();
-
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
-
 app.UseCors("CorsPolicy");
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.UseExceptionHandler();
-
 app.Run();
